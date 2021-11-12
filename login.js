d43498bef94ab48b8379b34086d296b770f878f3
AlnaLouis <93535716+AlnaLouis@users.noreply.github.com>
2021-11-12 17:22:15 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,76 +1,44 @@
-let userName = document.getElementById("txtUserName");
-let email = document.getElementById("txtEmail");
-let pwd = document.getElementById("txtPwd");
-let form = document.querySelector("form");
-let phone=document.getElementById("phone");
-function validateInput(){
-    console.log("validate input");
-    //check user name is empty
-    if(userName.value.trim()===""){
-       onError(userName,"User Name cannot be empty");
-    }
-    else{
-       onSuccess(userName)
-    }
-    if(phone.value.trim()===""){
-        onError(phone,"Phone Number cannot be empty");
-     }
-     else{
-        onSuccess(phone)
-     }
-    if(email.value.trim()===""){
-        onError(email,"Email cannot be empty");
-     }
-     else{
-         if(isValidEmail(email.value.trim())){
-            onError(email,"Email is not valid");
-     }else{
-         onSuccess(email);
-     }
+let uname = document.getElementById("uname");
+let pwd = document.getElementById("pwd");
 
-    }
-    if(pwd.value.trim()===""){
-        onError(email,"Email cannot be empty");
-     }
-     else{
-         onSuccess(pwd);
-     }
-    if(conPwd.value.trim()===""){
-        onError(conPwd,"Password cannot be empty")
-    }
-    else{
-        if(pwd.value.trim()!==conPwd.value.trim()){
-            onError(conPwd,"Password and confirm password doesn't match");
+function validateUser()
+{
+  if(uname.value=="")
+     {
+        error.innerHTML="The email id is in valid recheck the format";
+        error.style.color="red";
+        phone.style.border="2px solid red";
+        return false;
+       
         }
-        else{
-        onSuccess(conPwd);
+      else
+        {
+            phone.style.border="2px solid green";
+            return validateUse();
         }
-    }
-}
     
-
-document.querySelector("button")
-.addEventListener("click",(event)=>{
-    event.preventDefault();
-validateInput();
-});
- function onSuccess(input,input){
-    let parent=input.parentElement;
-    let messageEle=parent.querySelector("small");
-    messageEle.style.visibility="hidden";
-    parent.classList.remove("error");
-    parent.classList.add("Success");
+        
 }
 
- function onError(input,message){
-    let parent=input.parentElement;
-    let messageEle=parent.querySelector("small");
-    messageEle.style.visibility="visible";
-    messageEle.innerText=message;
-    parent.classList.add("error");
-    parent.classList.remove("Success");
+function validateUse()
+{
+    if (value.pwd.trim()=="") 
+    {
+        pwd.style.border="2px solid green";
+        return validatePwd();
+    }
+    else if(value.pwd.length<5) {
+        error.innerHTML="The username so small";
+        error.style.color="orange";
+        pwd.style.border="2px solid orange";
+        return false; 
+
+    }
+    else
+    {
+        error.innerHTML="The username is not valid";
+        error.style.color="red";
+        pwd.style.border="2px solid red";
+        return false; 
+    }
 }
-   functionisValidEmail(email)
-   {
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1-3\.}[0-9]{1,3}\.[0-9]{1,3}.[0-9]{1,3}\])|((a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.text(email);
-   } 
